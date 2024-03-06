2a5f62a0fbaaf76f3b0c6d07f89dd1f03b7ed870
NoctemMorphei <160981034+NoctemMorphei@users.noreply.github.com>
2024-03-06 16:57:34 -0300

    Create inf.py

---
+++ inf.py
@@ -0,0 +1,17 @@
+import cr 
+
+import cr
+
+inf1=(f'''
+{cr.bpurple} 
+
+
+ Google Dorking, também conhecido como Google Hacking, é uma técnica de pesquisa avançada que utiliza operadores de busca específicos para encontrar informações sensíveis e vulnerabilidades em websites e sistemas.
+ 
+ Os hackers podem usar essa técnica para encontrar documentos confidenciais, credenciais de login expostas, falhas de segurança e outras informações sensíveis que normalmente não seriam acessíveis através de uma pesquisa comum no Google. No entanto, o uso de técnicas de Google Dorking para explorar vulnerabilidades e obter informações sensíveis pode ser ilegal e violar as leis de privacidade e segurança cibernética, dependendo das circunstâncias e da legislação local. Isso pode levar a consequências legais, como processos judiciais e acusações criminais. É importante ressaltar que o Google Dorking deve ser utilizado somente para fins educacionais e de pesquisa legítima, e não para atividades ilegais. 
+ Além disso, o Google pode rastrear atividades suspeitas e colaborar com autoridades legais para identificar infratores. Portanto, é essencial agir de forma ética e responsável ao utilizar essa ferramenta.
+
+ 
+{cr.white} 
+
+''')
