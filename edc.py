2658b7068c079f6e871a7628a83a48b87d3c3a56
NoctemMorphei <160981034+NoctemMorphei@users.noreply.github.com>
2024-03-06 16:58:42 -0300

    Create edc.py

---
+++ edc.py
@@ -0,0 +1,25 @@
+import cr
+
+criadores1=f'''
+{cr.bpurple}
+
+
+
+
+                 V
+               (o o)
+              (  V  )
+|==============_m.m_===============|
+|   EQUIPE DE CRIAÇÃO E CONTATOS   |
+|==================================|
+|criador:NoctemMorphei             |
+|discord:NoctemMorphei             |
+|github:NoctemMorphei              |
+|----------------------------------|
+|contribuidor especial↓            |
+|nome:Phant0m The Great            | 
+|discord:phant0mthegreat           |
+|github:Phant0mthegreat            |
+|==================================|
+{cr.white}
+'''
