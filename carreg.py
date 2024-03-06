dca054a61c58eb2165285eb818860c0211186166
NoctemMorphei <160981034+NoctemMorphei@users.noreply.github.com>
2024-03-06 17:01:51 -0300

    Create carreg.py

---
+++ carreg.py
@@ -0,0 +1,6 @@
+import cr, banners, time, os
+def carreg():
+  os.system('clear')
+  print(f'{cr.bpurple}{banners.black}{cr.white}')
+  print(f"{cr.bpurple} bem vindo ao Dorking corvus oculis")
+  time.sleep(4)
